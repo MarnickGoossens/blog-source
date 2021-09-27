b47997cd7b36180491ce2bb7af0cec3e6dd63c59
Marnick Goossens <marnick.goossens.04@gmail.com>
2021-09-27 14:46:33 +0200

    pelican setup compleet

---
+++ src/pelicanconf.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = "Marnick Goossens"
+SITENAME = "Blog 6IICT"
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = "Europe/Paris"
+
+DEFAULT_LANG = "Dutch"
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (("Pelican", "https://getpelican.com/"),)
+
+# Social widget
+SOCIAL = (
+    ("You can add links in your config file", "#"),
+    ("Another social link", "#"),
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
